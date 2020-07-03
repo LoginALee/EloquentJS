2ecd07b22e71b4f97cac206680fff2fe4397d434
LoginALee <you@example.com>
2020-07-03 11:26:15 -0500

    Capitulo 11

--- Chapter 11.js
+++ Chapter 11.js
@@ -25,7 +25,16 @@ async function locateScalpel(nest) {
 
   function Promise_all(promises) {
     return new Promise((resolve, reject) => {
-      
+      let results = [];
+      let pending = promises.length;
+      for (let i = 0; i < promises.length; i++) {
+        promises[i].then(result => {
+          results[i] = result;
+          pending--;
+          if (pending == 0) resolve(results);
+        }).catch(reject);
+      }
+      if (promises.length == 0) resolve(results);
     });
   }
   
