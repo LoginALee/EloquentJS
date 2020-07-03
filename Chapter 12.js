9d8570c00929c9ffe3d7c6265647f4f8dc1ba6d9
LoginALee <you@example.com>
2020-07-03 14:54:17 -0500

    Chapter 12

--- Chapter 12.js
+++ Chapter 12.js
@@ -26,5 +26,16 @@ topScope.array = (...elements) => {
    `);
    // → 6
 
+   function skipSpace(string) {
+    let skippable = string.match(/^(\s|#.*)*/);
+    return string.slice(skippable[0].length);
+  }
+  
+  console.log(parse("# hello\nx"));
+  // → {type: "word", name: "x"}
+  
+  console.log(parse("a # one\n   # two\n()"));
+  // → {type: "apply",
+  //    operator: {type: "word", name: "a"},
+  //    args: []}
 
-   
